f3c7703e3d7d5f81b419298c35caacba20d2b493
ahask93 <ahask93@gmail.com>
2024-06-29 16:34:07 -0400

    first draft

--- main.java
+++ main.java
@@ -1,6 +1,50 @@
+import java.util.*;
+import java.io.*;
+
 public class Main {
     
-    private static void main(String[] args){
-        
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Java source file: ");
+        String filename = input.nextLine();
+
+        File file = new File(filename);
+        if (file.exists()) {
+           checkPairs(file);
+        } else {
+            System.out.println("File " + filename + " does not exist");
+        }
     }
+
+    public static Boolean checkPairs(File file) throws Exception {
+        int openParentheses = 0;
+        int openBrackets = 0;
+        int closeParentheses = 0;
+        int closeBrackets = 0;
+    
+        Scanner input = new Scanner(file);
+    
+        Boolean correct = false;
+        while (input.hasNext()){
+            String line = input.nextLine();
+            String[] words = line.split("");
+            for (int i = 0; i <= words.length; i++){
+                if (words[i].contains("(")) {
+                    openParentheses++;
+                } else if (words[i].contains(")")) {
+                    closeParentheses++;
+                } else if (words[i].contains("{")) {
+                    openBrackets++;
+                } else if (words[i].contains("}")) {
+                    closeBrackets++;
+                } 
+            }
+            if (openParentheses == closeParentheses && openBrackets == closeBrackets) {
+                correct = true;
+            }
+        }
+
+        return correct;
+    }
+        
 }
